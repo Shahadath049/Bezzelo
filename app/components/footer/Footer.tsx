ea0085eb0f1b92fc3d74ff23a5b93691242fa470
Shahadath049 <zamirshahadath049@gmail.com>
2024-01-29 10:04:01 +0600

    first commit

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import Container from "../Container";
+import FooterList from "./FooterLIst";
+import { FaRegCopyright } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { IoLogoYoutube } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa6";
+
+
+
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <Container>
+        <div
+          className="flex flex-col 
+            md:flex-row
+            justify-between
+            pt-16 pb-8"
+        >
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+            <Link href={"#"}>Dress</Link>
+            <Link href={"#"}>Dress</Link>
+            <Link href={"#"}>Dress</Link>
+            <Link href={"#"}>Dress</Link>
+            <Link href={"#"}>Dress</Link>
+            <Link href={"#"}>Dress</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Customer Service</h3>
+            <Link href={"#"}>Contact Us</Link>
+            <Link href={"#"}>Shipping Policy</Link>
+            <Link href={"#"}>Returns & Exchanges</Link>
+            <Link href={"#"}>Watches</Link>
+            <Link href={"#"}>FAQs</Link>
+          </FooterList>
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+            <h3 className="text-base font-bold mb-2">About Us</h3>
+            <p className="mb-2">
+              At our store , we are dedicated to providing the latest and
+              greatest fashion products and accessories to out customers. With a
+              wide selection of dresses,clothes,bags and other accessories.
+            </p>
+            <p className="mb-2">
+              <FaRegCopyright className="m-0 p-0"></FaRegCopyright> {new Date().getFullYear()} Bezzelo. All rights reserved
+            </p>
+          </div>
+          <FooterList>
+          <h3 className="text-base font-bold mb-2">About Us</h3>
+          <div className="flex gap-2">
+          <Link href={"#"}><FaFacebook /></Link>
+          <Link href={"#"}><AiFillTwitterCircle /></Link>
+          <Link href={"#"}><IoLogoYoutube /></Link>
+          <Link href={"#"}><FaInstagram /></Link>
+          </div>
+          </FooterList>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import NavBar from './components/nav/NabBar'
+import Footer from './components/footer/Footer'
+
+const poppins = Poppins({ subsets: ['latin'] ,weight:['400','700']})
+
+export const metadata: Metadata = {
+  title: 'Bezzelo',
+  description: 'Ecommerce app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} text-slate-700`}>
+        <div className='flex flex-col min-h-screen'>
+          <NavBar></NavBar>
+          <main className='flex-grow'>{children}</main>
+          <Footer></Footer>
+        </div> 
+        
+        
+        </body>
+    </html>
+  )
+}
